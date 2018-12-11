13137b0c78d9aeb38cc1accaefe891af039ced55
joannechen1223 <b04701232@ntu.edu.tw>
2018-12-11 19:23:48 +0800

    install redux and router

---
+++ client/src/components/js/TripPlannerApp.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+
+class TripPlannerApp extends Component {
+  constructor() {
+    super();
+  }
+
+  render() {
+    return (
+      <div>
+        hello!
+      </div>
+    );
+  }
+}
+
+export default TripPlannerApp;
